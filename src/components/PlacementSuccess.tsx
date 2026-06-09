f7ae7c107c3d5aae1cb8534105613815253fe4f3
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-06-09 14:39:39 +0530

    logo issue solved

--- src/components/PlacementSuccess.tsx
+++ src/components/PlacementSuccess.tsx
@@ -4,14 +4,14 @@ import { motion } from 'framer-motion';
 
 export const PlacementSuccess = () => {
   const companies = [
-    { name: 'TCS',          src: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Tata_Consultancy_Services_Logo.svg' },
+    { name: 'TCS',          src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Tata_Consultancy_Services_old_logo.svg/1280px-Tata_Consultancy_Services_old_logo.svg.png?_=20210617123944' },
     { name: 'Infosys',      src: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Infosys_logo.svg' },
     { name: 'Accenture',    src: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Accenture.svg' },
     { name: 'Wipro',        src: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Wipro_Primary_Logo_Color_RGB.svg' },
     { name: 'Cognizant',    src: 'https://upload.wikimedia.org/wikipedia/commons/4/43/Cognizant_logo_2022.svg' },
     { name: 'Capgemini',    src: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Capgemini_201x_logo.svg' },
-    { name: 'HCL',          src: 'https://upload.wikimedia.org/wikipedia/commons/0/05/HCL_Technologies_logo.svg' },
-    { name: 'Tech Mahindra',src: 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Tech_Mahindra_New_Logo.svg' },
+    { name: 'HCL',          src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/HCL_Technologies_logo.svg/1920px-HCL_Technologies_logo.svg.png?_=20100214041014' },
+    { name: 'Tech Mahindra',src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/HCL_Technologies_logo.svg/1920px-HCL_Technologies_logo.svg.png?_=20100214041014' },
   ];
 
   const placements = [
